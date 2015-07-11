60253e544bcb65b375acfbd48e2a0b4b20e4f23e
Atsushi Odagiri <aodagx@gmail.com>
2015-07-11 16:38:44 +0900

    prep for 0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 __author__ = 'Atsushi Odagiri'
 __author_email__ = 'aodagx@gmail.com'
-__version__ = '0.0'
+__version__ = '0.1'
 
 
 here = os.path.dirname(__file__)
@@ -31,8 +31,14 @@ setup(
     url='https://github.com/rebeccaframework/rebecca.annotationmapper',
     namespace_packages=["rebecca"],
     description='annotation view mapper for pyramid',
-    long_description=read("README.rst"),
+    long_description=read("README.rst") + "\n" + read('CHANGES.rst'),
     install_requires=requires,
     packages=find_packages(),
     test_suite="rebecca.annotationmapper",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Framework :: Pyramid",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
 )
